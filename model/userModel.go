49f37d6658992178f71bf150ae28202db8b40ed1
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-02-22 21:37:54 +0800

    fix: add json in user model

--- model/userModel.go
+++ model/userModel.go
@@ -1,11 +1,17 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
-	gorm.Model
-	ID       uint `gorm:"primary_key"`
-	Username string
-	Password string
-	Email    string `gorm:"unique"`
+	ID        uint           `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+	Username  string         `json:"username"`
+	Password  string         `json:"password"`
+	Email     string         `gorm:"unique" json:"email"`
 }
