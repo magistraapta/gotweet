e362e49cb075e49b1b4fb6098f17a5f00d8162fa
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-02-23 19:10:50 +0800

    feat: add user CRUD

--- controllers/userController.go
+++ controllers/userController.go
@@ -4,12 +4,20 @@ import (
 	"docker-test/initializers"
 	"docker-test/model"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
-const bcryptCost = 10
+const (
+	MaxUploadSize   = 8 << 20
+	FileDirectory   = "files"
+	filePermissions = 0o755
+	bcryptCost      = 10
+)
 
 func Signup(c *gin.Context) {
 	// get input from body
@@ -86,4 +94,218 @@ func Login(c *gin.Context) {
 			"error": "invalid email or password",
 		})
 	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to create token: " + err.Error(),
+		})
+		return
+	}
+
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
+
+	c.JSON(http.StatusOK, gin.H{
+		"token": tokenString,
+	})
+}
+
+func Validate(c *gin.Context) {
+
+	user, _ := c.Get("user")
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Im logged in!",
+		"user":    user,
+	})
+}
+
+func UpdateUser(c *gin.Context) {
+	// get current user
+	user, exist := c.Get("user")
+
+	if !exist {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "Not Authenticated",
+		})
+
+		return
+	}
+
+	// get user req body
+	var body struct {
+		Username string
+	}
+
+	if err := c.Bind(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+
+		return
+	}
+
+	// update user
+	currentUser := user.(model.User)
+
+	result := initializers.DB.Model(&currentUser).Updates(map[string]interface{}{
+		"username": body.Username,
+	})
+
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to update user" + result.Error.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "User updated",
+		"user":    currentUser,
+	})
+
+}
+
+func HandleImageUpload(c *gin.Context) {
+	file, err := c.FormFile("image")
+	if err != nil {
+		c.HTML(http.StatusBadRequest, "index.html", gin.H{
+			"error": "Failed to upload file",
+		})
+	}
+
+	if file.Size > MaxUploadSize {
+		c.HTML(http.StatusInternalServerError, "index.html", gin.H{
+			"error": "file size above limit",
+		})
+	}
+
+	if err := c.SaveUploadedFile(file, "static/upload/"+file.Filename); err != nil {
+		c.HTML(http.StatusBadRequest, "index.html", gin.H{
+			"error": "Failed to upload file",
+		})
+	}
+
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"image": "static/upload/" + file.Filename,
+	})
+}
+
+func GetAllUsers(c *gin.Context) {
+	var users []model.User
+
+	result := initializers.DB.Find(&users)
+
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg":   "Failed to retrieve users",
+			"error": result.Error.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"msg":   "Success retrieved all users",
+		"users": users, // Return the users array, not the result object
+	})
+}
+
+func GetUserById(c *gin.Context) {
+	// get user id
+	id := c.Param("id")
+
+	// get user from db by id
+	var user model.User
+
+	result := initializers.DB.First(&user, id)
+
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": result.Error.Error(),
+		})
+		return
+	}
+
+	// response
+	c.JSON(http.StatusOK, gin.H{
+		"msg":  "Successfully getting user",
+		"user": user,
+	})
+}
+
+func DeleteUserById(c *gin.Context) {
+	// get user id
+	id := c.Param("id")
+
+	// query to delete user
+	var user model.User
+	result := initializers.DB.Delete(&user, id)
+
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to delete user: " + result.Error.Error(),
+		})
+		return
+	}
+
+	if result.RowsAffected == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "User not found",
+		})
+	}
+	// send response
+	c.JSON(http.StatusOK, gin.H{
+		"msg": "Successfully delete user",
+	})
+}
+
+func EditUserById(c *gin.Context) {
+	// Get user ID from URL parameter
+	id := c.Param("id")
+
+	// Parse request body
+	var body struct {
+		Username string
+	}
+	if err := c.BindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid request body",
+		})
+		return
+	}
+
+	// Find user first
+	var user model.User
+	result := initializers.DB.First(&user, id)
+
+	if result.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "User not found",
+		})
+		return
+	}
+
+	// Update the user
+	result = initializers.DB.Model(&user).Update("username", body.Username)
+
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": result.Error.Error(),
+		})
+		return
+	}
+
+	// Send response
+	c.JSON(http.StatusOK, gin.H{
+		"msg":  "Successfully edited user",
+		"user": user,
+	})
 }
