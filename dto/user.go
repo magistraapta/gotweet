2728575c6cac0b968c3c02aba516b68df52d6523
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-03-10 21:05:35 +0800

    fix: add user dto

--- dto/user.go
+++ dto/user.go
@@ -1,6 +1,14 @@
 package dto
 
+import "time"
+
 type UserLoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type UserResponse struct {
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
 }
