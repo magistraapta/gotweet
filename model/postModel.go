e2528ef4f598b56d5315af2d0167d1f33e96f29c
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-03-12 21:33:05 +0800

    fix: add ID property

--- model/postModel.go
+++ model/postModel.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type Post struct {
 	gorm.Model
+	ID       uint
 	UserID   uint
 	User     User      `gorm:"foreignKey:UserID"`
 	Comments []Comment `gorm:"foreignKey:PostID;constraint:OnDelete:CASCADE;"`
